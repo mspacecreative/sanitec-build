68e8dca4e434a5386d1e71e00623db2994f74cf5
Matt <matt@mspacecreative.com>
2020-06-07 13:17:40 -0300

    c

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -33,17 +33,8 @@
 	
 	// SMOOTH SCROLL
 	// Select all links with hashes
-	$('a[href*=#]:not([href=#])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top - 50
-				}, 1000);
-	        return false;
-			}
-	    }
+	$('.mobile-nav a[href*=\\#]').click(function() {
+		$('body, #page-container').toggleClass('is-active');
 	});
 	
 })(jQuery);
